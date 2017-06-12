be99cc0adf8683e06b60471ba6ab0e54bba38faa
N REZA <nreza@Ns-MacBook-Pro.local>
2017-06-12 06:22:15 +0600

    With Preloader and working scripts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # nude
-For Super Fast Web Coding
+Personal Barebone WebPlate for Super Fast Web Coding
 
 ## Media Query Snippet
 ```css
@@ -17,4 +17,15 @@ body {}
 ```css
 /* font-style font -weight font-size/line-height 'font-family', 'font-family', fallback */
 font: normal 300 0.9em/1.3 'Lato', 'Helvetica Neue', sans-serif;
+```
+
+## For using Rellax js
+
+```html
+<div class="rellax" data-rellax-speed="7" data-rellax-percentage="0.5">
+  I’m super fast!! And super centered!!
+</div>
+<div class="rellax" data-rellax-speed="-4" data-rellax-percentage="0.5">
+  I’m extra slow and smooth, and hella centered.
+</div>
 ```
--- js/allScripts.js
+++ js/allScripts.js
@@ -4,12 +4,12 @@ jQuery(document).ready(function() {
 // Viewport Checker Animations
 
 	//sample animation
-	jQuery('.vp-theBounceUp').addClass("space-invisible").viewportChecker({
-		    classToRemove: 'space-invisible', // Class to remove before adding 'classToAdd' to the elements
-		    classToAdd: 'space-visible nuM-theBounceUp', // Class to add to the elements when they are visible,
-		    removeClassAfterAnimation: false, // Remove added classes after animation has finished
-		   	repeat: true, // Add the possibility to remove the class if the elements are not visible
-		    scrollHorizontal: false // Set to true if your website scrolls horizontal instead of vertical.
+	jQuery('.vp-BlurIn').addClass("space-invisible").viewportChecker({
+		    classToRemove: 'space-invisible',
+		    classToAdd: 'space-visible motion-BlurIn', 
+		    removeClassAfterAnimation: false, 
+		   	repeat: true, 
+		    scrollHorizontal: false
 	});
 	//--
 	
